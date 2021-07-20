9853ac3d6ef0be56df5f0a6d8adfba1a4122c256
ashutosh <ashu.4chaudhari@gmail.com>
2021-07-20 23:43:22 +0530

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:webresponsivedesign1/controllers/countercontroller.dart';
+import 'package:webresponsivedesign1/screens/other.dart';
+
+class HomeScreen extends StatelessWidget {
+  final CounterController counterController = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text("Clicks: ${counterController.counter.value}")),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Get.to(OtherScreen());
+                },
+                child: Text("Open Other Screen")),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counterController.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/screens/other.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:webresponsivedesign1/controllers/countercontroller.dart';
+
+class OtherScreen extends StatelessWidget {
+  final CounterController _counterController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Screen was clicked ${_counterController.counter.value} times"),
+          SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              Get.back();
+
+            }, child: Text("Open Other Screen"))],
+        
+      ),
+    );
+  }
+}
